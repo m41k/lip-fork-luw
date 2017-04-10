93f1c9683fb96cf266332798f92e28845c64a900
m41k <maik.alberto@hotmail.com>
2017-04-10 00:14:07 -0300

    Delete vps-iptables.sh

--- opt/lip/tools/vps-iptables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#example
-#iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 81 -j DNAT --to 10.0.3.86:80
-
-/sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $1 -j DNAT --to $2:$3
-
